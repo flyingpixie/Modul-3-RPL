08ebe048030e57ce989c5c6c3bc8eec3420ac615
Triana Agista <email.anda@gmail.com>
2026-09-25 01:47:22 +0800

    Menambahkan file program python sederhana

---
+++ Tugas3.py
@@ -0,0 +1,10 @@
+def sapa_pengguna():
+    print("=== Program Sederhana Tugas Modul 3 ===")
+    nama = input("Masukkan nama Anda: ")
+    nim = input("Masukkan NIM Anda: ")
+    
+    print(f"\nHalo, {nama} dengan NIM {nim}!")
+    print("Selamat Anda telah berhasil menjalankan program Python dan menghubungkannya ke GitHub.")
+
+if __name__ == "__main__":
+    sapa_pengguna()
